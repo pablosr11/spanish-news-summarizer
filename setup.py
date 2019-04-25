9566b9f5ff2f42257004305102ab84fa3febe891
sandori11 <pablosr11@gmail.com>
2019-04-25 12:30:43 +0100

    basic structure - initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+ 2019 Pablo Sanderson Ramos | spanish-news-summarizer
+"""
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Cortito',
+    version='0.0.1',
+    description='Find summaries of your favourite news outlet',
+    long_description=readme,
+    author='Pablo Sanderson Ramos',
+    author_email='pablosr11@gmail.com',
+    url='https://github.com/pablosr11/spanish-news-summarizer',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
