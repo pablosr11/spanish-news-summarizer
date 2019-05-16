15b9f4fe387b627036a82d1c6aa5c1b8b99baeff
sandori11 <pablosr11@gmail.com>
2019-05-16 16:29:30 +0100

    Changed name

--- forocanario.py
+++ forocanario.py
@@ -5,6 +5,3 @@ from app.models import Article, Words, Article_NLP, Article_Comments
 def make_shell_context():
     return {'db': db, 'Article': Article, 'Words': Words,
     'Article_Comments':Article_Comments, 'Article_NLP': Article_NLP}
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
